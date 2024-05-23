0de008fae73b479765819910e4cbf38681de9d4f
mahamaya-mishra <87880693+mahamaya-mishra@users.noreply.github.com>
2024-05-23 23:27:43 +0530

    Time: 278 ms (18.44%) | Memory: 22.5 MB (10.22%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -27,18 +27,35 @@ if(root ==NULL){
 
 
     }
-    int diameterOfBinaryTree(TreeNode* root) {
-        if(root==NULL){
+        int diameterOfBinaryTree1(TreeNode* root, int &maxi) {
+if(root==NULL){
             return 0;
         }
-int dia1 = diameterOfBinaryTree(root->left);
-int dia2 = diameterOfBinaryTree(root->right);
-
-
-        int mxdL = maxDepth(root->left);
+           int mxdL = maxDepth(root->left);
         int mxdR = maxDepth(root->right); 
-        int h1= mxdL +mxdR;
+       maxi= max(maxi, mxdR+mxdL);
+      int m = diameterOfBinaryTree1(root->left , maxi);
+      int l =        diameterOfBinaryTree1(root->right , maxi);
+
+maxi= max(maxi, max(m, l));
+        return maxi;
+        }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        // if(root==NULL){
+        //     return 0;
+        // }
+// int dia1 = diameterOfBinaryTree(root->left);
+// int dia2 = diameterOfBinaryTree(root->right);
+
+
+        // int mxdL = maxDepth(root->left);
+        // int mxdR = maxDepth(root->right); 
+        // int h1= mxdL +mxdR;
       
-        return max(h1 ,max(dia1, dia2));
+        // return max(h1 ,max(dia1, dia2));
+int max=-1;
+         
+        return diameterOfBinaryTree1(root, max);
     }
 };
