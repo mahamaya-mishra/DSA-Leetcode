f2af6813f107f7480c044ab64873c8a14f602d29
mahamaya-mishra <87880693+mahamaya-mishra@users.noreply.github.com>
2024-06-30 02:37:21 +0530

    Time: 2 ms (55.13%) | Memory: 7.1 MB (24.16%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
+ string inString(int n) {
+    if (n == 0) return "0";
+
+    string b = "";
+    while (n > 0) {
+        b = (n % 2 == 0 ? "0" : "1") + b;
+        n = n / 2;
+    }
+    return b;
+}
     int getSum(int a, int b) {
-    while(b!=0){
+        while(b!=0){
             int carry=(a&b)<<1;
             a=a^b;
             b=carry;
