08d317670b29a33c849669eceba6a952369addf0
mahamaya-mishra <87880693+mahamaya-mishra@users.noreply.github.com>
2024-07-03 18:25:30 +0530

    Time: 2 ms (76.76%) | Memory: 8.9 MB (80.37%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-
-  int hammingWeight(int n) {
-
-int res=0;
-
-        while(n){
-res+= n%2;
-n=n>>1;
-        }
-        return res;
-    }
     vector<int> countBits(int n) {
-        if (n == 0) {
-            return {0};
-        }
-        
-        vector<int> ans= countBits(n-1);
-        int a= hammingWeight(n);
-        ans.push_back(a);
-        return ans;
+        vector<int> dp(n+1, -1);
+        int offset =1;
+        dp[0]=0;
+        for(int i=1; i<=n; i++){
 
-        
+         if(2*offset==i){
+            offset=i;
+
+         }
+        dp[i]= 1+ dp[i-offset];
+
+        }
+        return dp;
     }
 };
